ec2a0974c45ca34601f99458dcf1ade5023bfa8f
abdulbasit <imabk10@gmail.com>
2022-10-11 18:38:57 +0530

    api to update vehicles

--- BackEnd/route/add_remove_Update_VehicleRoute.js
+++ BackEnd/route/add_remove_Update_VehicleRoute.js
@@ -128,4 +128,72 @@ vehicleRoute.get(`${api}/getVehicle`, async (req, res) => {
     return res.status(500).send("Internal Server Error");
   }
 });
+
+// to update vehicle
+vehicleRoute.patch(`${api}/updateVehicle/:id`, getUserId, async (req, res) => {
+  let success = false;
+  const id = req.params.id;
+
+  try {
+    // to find vehicle to edit
+    const findVehicle = await Vehicles.findById({ _id: ObjectId(id) });
+    if (!findVehicle) {
+      return res.status(400).json({ success, message: "Vehicle not found" });
+    }
+
+    // verifying owner
+    if (JSON.stringify(req.userId) !== JSON.stringify(findVehicle.owner)) {
+      return res
+        .status(400)
+        .json({ success, message: "Your are not eligible to edit" });
+    }
+
+    // finding the vehicle with same number
+    // const registeredVehicle = await Vehicles.findOne({
+    //   vNumber: req.body.vNumber,
+    // });
+    // console.log(registeredVehicle);
+
+    // if (registeredVehicle) {
+    //   return res.status(400).json({
+    //     success,
+    //     message: "This vehicle with this registered number is already there",
+    //   });
+    // }
+
+    if (req.body.type === "") {
+      return res.status(400).json({ message: "Type should not be empty" });
+    }
+    if (req.body.vNumber === "") {
+      return res
+        .status(400)
+        .json({ message: "Enter the registered number for vehicle" });
+    }
+    if (req.body.capacity === "") {
+      return res.status(400).json({ message: "Enter the capacity of vehicle" });
+    }
+    if (req.body.initialPrice === "") {
+      return res
+        .status(400)
+        .json({ message: "Enter the initialPrice of vehicle" });
+    }
+    const updatedVehicle = await Vehicles.findByIdAndUpdate(
+      { _id: id },
+      {
+        type: req.body.type,
+        vNumber: req.body.vNumber,
+        capacity: req.body.capacity,
+        initialPrice: req.body.initialPrice,
+      },
+      { new: true }
+    );
+    success = true;
+    return res
+      .status(200)
+      .json({ success, message: "Vehicle Updated Successfully" });
+  } catch (error) {
+    console.log("update Vehicle error", error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 module.exports = vehicleRoute;
