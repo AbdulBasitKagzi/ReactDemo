cf978c2a0641066c3e46cf7af0d407c2374a2cc9
abdulbasit <imabk10@gmail.com>
2022-10-13 19:00:56 +0530

    added route for payment

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,6 +6,7 @@ const customerUserRoute = require("./route/customerUserRoute");
 const vehicleRoute = require("./route/add_remove_Update_VehicleRoute");
 const goodsRoute = require("./route/add_delete_goods");
 const orderRoute = require("./route/orders_route");
+const paymentRoute = require("./route/payment");
 
 require("dotenv").config({ path: "config.env" });
 
@@ -15,14 +16,17 @@ const app = express();
 // setting up port
 const port = process.env.PORT || 5000;
 
-// setting up route
 app.use(cors());
+
 app.use(express.json());
+
+// setting up route
 app.use(route);
 app.use(customerUserRoute);
 app.use(vehicleRoute);
 app.use(goodsRoute);
 app.use(orderRoute);
+app.use(paymentRoute);
 
 connectToMongo();
 // starting up the server
---
+++ BackEnd/route/payment.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
+require("dotenv").config({ path: "config.env" });
+
+const paymentRoute = express.Router();
+
+const api = process.env.API;
+
+// order api
+paymentRoute.post(`/api/payment/orders`, async (req, res) => {
+  try {
+    const instance = new Razorpay({
+      key_id: process.env.KEY_ID,
+      key_secret: process.env.KEY_SECRET,
+    });
+    const options = {
+      amount: 50000,
+      currency: "INR",
+      receipt: crypto.randomBytes(10).toString("hex"),
+    };
+    instance.orders.create(options, (error, order) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send("Something went wrong");
+      }
+      res.status(200).json({ data: order });
+    });
+  } catch (error) {
+    console.log("catcherror", error);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+// payment verify api
+paymentRoute.post(`/api/payment/verify`, async (req, res) => {
+  try {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
+    const sign = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSign = crypto
+      .createHmac("sha256", process.env.KEY_SECRET)
+      .update(sign.toString())
+      .digest("hex");
+
+    if (razorpay_signature === expectedSign) {
+      return res.status(200).json({ message: "Payment verified successfully" });
+    } else {
+      return res.status(400).json({ message: "Payment declined" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+module.exports = paymentRoute;
