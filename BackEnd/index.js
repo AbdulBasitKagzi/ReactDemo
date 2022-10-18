cbd3738d77f34793217fa5aca2add656d849b54f
abdulbasit <imabk10@gmail.com>
2022-10-18 11:28:50 +0530

    uploading to heroku

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const route = require("./route/route");
 const connectToMongo = require("./db/db");
 const cors = require("cors");
@@ -17,9 +18,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname + "/public")));
 // setting up route
 app.use(route);
 app.use(customerUserRoute);
